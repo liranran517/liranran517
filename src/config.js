bc267a407a55147a1bd3375a4d3d17c9797906e3
lovelywen <loveweni@qq.com>
2025-11-03 17:22:57 +0800

    init

---
+++ src/config.js
@@ -0,0 +1,83 @@
+export default {
+  // page1部分
+  page1: {
+    titleEn: 'Deep Learning System Development Engineer', //英文标题
+    title: '深度学习系统研发工程师', //中文标题
+  },
+  // page2部分
+  page2: {
+    authorImg: 'page1.jpg', // 作者头像
+    xinhui: '本人小橙，是一位深度学习系统研发工程师。', // 幸会
+    qiuzhi: '深度学习系统研发', // 求职意向
+    guanyuwo: '愿天下无BUG' // 关于我
+  },
+
+  // page3部分
+  page3: [{
+    icon: 'icon-tubiao-',
+    title: 'C++',
+    msg: ['C++基础', 'C++面向对象','C++多线程']
+  },{
+    icon: 'icon-diannao', // 图标
+    title: 'Python', // 标题
+    msg: ['Anaconda', 'PyTorch', 'TensorFlow'] //介绍
+  }, {
+    icon: 'icon-qianbi1',
+    title: 'CUDA',
+    msg: ['CUDA编程', 'GPU内核优化']
+  },  {
+    icon: 'icon-shouji',
+    title: '项目经验',
+    msg: ['计算机视觉','自然语言处理',]
+  }],
+  // page4部分
+  page4: {
+    // 我的历程
+    course: [{
+      date: '2022/9——至今', // 时间
+      desc: { // 经历
+        title: '河南师范大学',
+        list: ['概率论与数理统计', '高等代数', '离散数学', '分布式系统与并行计算', 'c++/python程序设计']
+      }
+    }],
+    // 我的拓展技能掌握
+    singlelist: [{
+      title: 'C++/python程序设计',
+      text: '掌握'
+    }, {
+      title: 'PyTorch/TensorFlow',
+      text: '熟练'
+    }, {
+      title: '概率论与数理统计/离散数学',
+      text: '精通'
+    }, {
+      title: '部署项目',
+      text: '掌握'
+    }, {
+      title: 'CUDA编程',
+      text: '了解'
+    }],
+    // 我的基本技能掌握
+    proresslist: [{
+      title: '概率论与数理统计/离散数学',
+      value: '90%'
+    }, {
+      title: 'PyTorch/TensorFlow',
+      value: '85%'
+    }, {
+      title: 'C++/python程序设计',
+      value: '70%'
+    }, {
+      title: '分布式系统与并行计算',
+      value: '60%'
+    }, {
+      title: 'CUDA',
+      value: '40%'
+    }]
+  },
+  // page6部分
+  page6: {
+    github: 'https://github.com/smallOrangei',
+    email: 'Mailto:liranran517@outlook.com?Subject=邮箱标题&Body=邮箱内容！'
+  }
+}
